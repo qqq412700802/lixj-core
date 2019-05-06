2953ada3b77fd32d7d27c912bf17e20777abd4d8
412700802@qq.com <q412700802>
2019-05-06 21:50:12 +0800

    Initial commit

---
+++ src/test/java/com/lixj/AppTest.java
@@ -0,0 +1,20 @@
+package com.lixj;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
