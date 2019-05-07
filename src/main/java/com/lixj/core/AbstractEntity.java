ae2bfe4385b28db377b182d1cacaaf24e3301fcf
412700802@qq.com <q412700802>
2019-05-07 12:16:35 +0800

    增加覆写注解

--- src/main/java/com/lixj/core/AbstractEntity.java
+++ src/main/java/com/lixj/core/AbstractEntity.java
@@ -7,32 +7,38 @@ public abstract class AbstractEntity
   private Long id;
   private Integer version = 0;
   private Long lastAccess = 0L;
-  
+
+  @Override
   public Long getId()
   {
     return this.id;
   }
   
+  @Override
   public void setId(Long id)
   {
     this.id = id;
   }
   
+  @Override
   public Integer getVersion()
   {
     return this.version;
   }
   
+  @Override
   public void setVersion(Integer version)
   {
     this.version = version;
   }
   
+  @Override
   public Long getLastAccess()
   {
     return this.lastAccess;
   }
   
+  @Override
   public void setLastAccess(Long lastAccess)
   {
     this.lastAccess = lastAccess;
